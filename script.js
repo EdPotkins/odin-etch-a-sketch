8e262c3c8b2076bed51965e44b099b8012601632
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-09 20:59:43 +0300

    Add hover effect squares change color

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 16*16; i++){
-
-const square = document.createElement("div");
-square.classList.add("square");
-container.appendChild(square);
-
+    const square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => square.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "purple";
+}));
